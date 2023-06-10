0af3bd0378afbf9cc6467b8854d1da22aafa77da
SGR <95160371+SuchGameR@users.noreply.github.com>
2023-06-11 01:33:11 +0900

    Add files via upload

---
+++ netatale2.js
@@ -0,0 +1,42 @@
+document.oncontextmenu = function(){
+  return false;
+}
+
+
+
+// ビジーwaitを使う方法
+function sleep(waitMsec) {
+  var startMsec = new Date();
+ 
+  // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+  while (new Date() - startMsec < waitMsec);
+}
+
+
+
+
+let index = 0;
+
+function printLetter() {
+  const message = messageContents;
+  const messageElement = document.getElementById("message");
+  messageElement.textContent += message[index];
+  index++;
+
+  if (index < message.length) {
+    setTimeout(printLetter, 100); // 1000ミリ秒（1秒）ごとに次の文字を表示
+  }
+}
+
+function startPrinting() {
+  index = 0;
+  messageElement.textContent = "";
+  printLetter();
+}
+
+messageContents = "＊ 殺ってやるか?";
+printLetter();
+
+setTimeout(() => {
+  location.href = 'netatale3.html';
+}, 4000);
